e0334ec8eddeb21c6505f14d1d075a7e9f6f05c3
kiran-jaswant <kiranjaswant08@gmail.com>
2025-04-18 14:35:33 +0530

    functionality added

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ form.addEventListener('submit', function (e) {
 
     e.preventDefault();
 
-    console.log(e)
 
     let h = document.querySelector("#height");
     let w = document.querySelector("#weight");
@@ -15,17 +14,61 @@ form.addEventListener('submit', function (e) {
     let ht = parseInt(h.value);
     let wt = parseInt(w.value);
 
-    if (ht === '' || ht < 0 || ht === NaN) {
-        result.innerHTML = " <h5> Please give valid height </h5>"
+    if (ht === '' || ht < 0 || isNaN(ht)) {
+        result.innerHTML = ' <h5> Please give valid height </h5>';
 
-    } else if (wt === '' || wt < 0 || wt === NaN) {
-        result.innerHTML = " <h5> Please give valid weight</h5>"
-        
+    } else if (wt === '' || wt < 0 || isNaN(wt)) {
+        result.innerHTML = ' <h5> Please give valid weight</h5>';
+
     } else {
 
         const bmi = (wt / ((ht * ht) / 10000)).toFixed(2);
 
-        result.innerHTML = `<b>${bmi} </b>`
+        result.innerHTML = `<b>${bmi} </b>`;
+
+        const messageDiv = document.createElement('div');
+        const img = document.createElement("img");
+
+        let secondpart = document.querySelector("#second-part")
+
+        if (bmi < 18.6) {
+            console.log("you are under weight");
+
+            messageDiv.className = 'message';
+            messageDiv.innerHTML = '<p>You are underweight.</p><p>Eat more to gain weight</p>';
+            img.src = './images/under-weight.avif';
+            img.alt = 'Underweight warning image';
+            img.className = 'message-image';
+
+            secondpart.appendChild(messageDiv);
+            secondpart.appendChild(img);
+            secondpart.classList.add('after-second-part');
+
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            console.log("your weight is normal")
+
+            messageDiv.className = 'message';
+            messageDiv.innerHTML = '<p>You are Normal.</p><p>Eat healthy and stay fit</p>';
+            img.src = './images/fitt.avif';
+            img.alt = 'Fitness image';
+            img.className = 'message-image';
+
+            secondpart.appendChild(messageDiv);
+            secondpart.appendChild(img);
+            secondpart.classList.add('after-second-part');
+        } else {
+            console.log("you are overweight")
+
+            messageDiv.className = 'message';
+            messageDiv.innerHTML = '<p>You are Overweight.</p><p>Eat less to stay fit</p>';
+            img.src = './images/over-weight.jpg';
+            img.alt = 'Overweight warning image';
+            img.className = 'message-image';
+
+            secondpart.appendChild(messageDiv);
+            secondpart.appendChild(img);
+            secondpart.classList.add('after-second-part');
+        }
 
     }
 
