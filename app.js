38ed4d44d8ae74af5c580efbba96b7841e675ad9
kiran-jaswant <kiranjaswant08@gmail.com>
2025-04-18 11:23:54 +0530

    styling added

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ form.addEventListener('submit', function (e) {
 
     let h = document.querySelector("#height");
     let w = document.querySelector("#weight");
-    
+
     let result = document.querySelector(".result");
 
     let ht = parseInt(h.value);
     let wt = parseInt(w.value);
 
+    if (ht === '' || ht < 0 || ht === NaN) {
+        result.innerHTML = " <h5> Please give valid height </h5>"
 
-    const bmi = (wt /((ht*ht)/10000)).toFixed(2);
-    
-    result.innerHTML = `<b>${bmi} </b>`
+    } else if (wt === '' || wt < 0 || wt === NaN) {
+        result.innerHTML = " <h5> Please give valid weight</h5>"
+        
+    } else {
+
+        const bmi = (wt / ((ht * ht) / 10000)).toFixed(2);
+
+        result.innerHTML = `<b>${bmi} </b>`
+
+    }
+
+
+
     1
 
 });
